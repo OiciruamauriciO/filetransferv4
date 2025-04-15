9922b8d6dcc074866bb90897d7707411ecc64657
UsuarioBootcampAndroid <tareas2024bootcamp@gmail.com>
2025-04-15 11:38:50 -0400

    cambios

--- entrada - ruta al 14042025/procesararchivocfgrisaexcel.py
+++ entrada - ruta al 14042025/procesararchivocfgrisaexcel.py
@@ -19,4 +19,4 @@ def texto_a_excel(nombre_archivo_txt, nombre_archivo_excel):
     print(f"Archivo Excel guardado como '{nombre_archivo_excel}'")
 
 # Ejemplo de uso:
-texto_a_excel('cfgris.250411_ajustado.txt', 'cfgris.250411_ajustado.xlsx')
+texto_a_excel('cfgris.250414_ajustado.txt', 'cfgris.250414_ajustado.xlsx')
